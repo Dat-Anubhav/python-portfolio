ce18ab8aa567a3e464d9d3571f6efb28fcf5712a
Dat-Anubhav <anubhavsrivastavbst@gmail.com>
2025-10-24 22:33:33 +0530

    Improve comments in exercise-03

--- python-workbook-exercises/section-03-loops-exercises/exercise-03-Nested-loop.py
+++ python-workbook-exercises/section-03-loops-exercises/exercise-03-Nested-loop.py
@@ -1,10 +1,10 @@
 '''Q3:- Write a program that prints a 5x5 grid of asterisks (*) using nested loops.'''
 
-# This for will take care of number of lines or rows 
+# This while will take care of number of lines
 i=1
 while (i<=5):
 
-# This for loop will take care of each line of ***** or columns
+# This for loop will take care of each line of *****
     for j in range(1):
 
         print('*****')
