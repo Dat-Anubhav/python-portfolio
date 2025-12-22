cdbb4e3ec2dd17b66c6e900c86b41b988b77b768
Dat-Anubhav <anubhavsrivastavbst@gmail.com>
2025-12-22 22:09:03 +0530

    Created SQL Server database and ran first queries

---
+++ Microsoft-SQL-Server-Basics/SQLQuery1.sql
@@ -0,0 +1,24 @@
+
+Create database Our_employee_details
+
+use Our_employee_details
+
+
+CREATE TABLE Employees
+(EmployeeID INT PRIMARY KEY,
+Firstname VARCHAR(30),
+Lastname VARCHAR(30),
+Department VARCHAR(30),
+Salary DECIMAL(10,2),
+HiredDate   DATE);
+
+
+INSERT INTO Employees (EmployeeID, Firstname, Lastname, Department, Salary, HiredDate)
+VALUES
+(1, 'Anubhav','Srivastav','Data Science','100000', '2022-01-15' ),
+(2, 'Rishu', 'Srivastav','Web Development', '120000', '2023-03-23'),
+(3, 'Minato', 'Namikaze','Finance', '150000', '2024-07-10'),
+(4, 'Ben', 'Tennyson', 'Marketing', '170000', '2025-09-30'),
+(5, 'Harry', 'Potter', 'IT', '200000', '2026-02-20');
+
+
---
+++ Microsoft-SQL-Server-Basics/SQLQuery2.sql
@@ -0,0 +1,15 @@
+use Our_employee_details
+
+Select * From Employees
+
+Select Firstname from Employees
+
+Select Firstname,Lastname from Employees
+
+Select EmployeesID(5) from Employees
+
+Select Firstname,lastname,Salary from Employees
+
+Select EmployeeID, Concat(Firstname,' ',Lastname) from Employees
+
+Select EmployeeId, Concat(Firstname,' ',Lastname) as [Employee_Name] from Employees
